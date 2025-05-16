f56bb14c691467053f14d29249aadd61245f6f7c
Bernardps <psbernard02@gmail.com>
2025-05-16 15:08:48 +0530

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,8 +1,15 @@
-void loop() {
-  timeClient.update();
-  delay(2000);
+#ifndef SECRETS_H
+#define SECRETS_H
 
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi not connected, skipping this cycle.");
-    return;
-  }
+// WiFi Credentials
+const char* WIFI_SSID = "REDMI";
+const char* WIFI_PASSWORD = "PASSWORD";
+
+// ThingSpeak
+unsigned long myChannelNumber = 2821073;
+const char* myWriteAPIKey = "SWOO6R2Z52ICIV52";
+
+// MongoDB API URL
+const char* serverUrl = "https://4q4hmaf83d.execute-api.ap-south-1.amazonaws.com/testing";
+
+#endif // SECRETS_H
