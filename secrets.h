99e9e9093fbebf656622ed8f99a5c1f600e828fa
Bernardps <psbernard02@gmail.com>
2025-05-16 13:49:36 +0530

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi Credentials
+const char* WIFI_SSID = "REDMI";
+const char* WIFI_PASSWORD = "PASSWORD";
+
+// ThingSpeak
+unsigned long THINGSPEAK_CHANNEL = 2821073;
+const char* THINGSPEAK_API_KEY = "SWOO6R2Z52IC
